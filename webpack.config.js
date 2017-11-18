e5a53525e2066ecf3601084ddedcdbf43ed065d4
Anton Aleksanenkov <a.aleksanenkov@crystals.ru>
2017-11-18 20:28:41 +0300

    moved from jquery to react and fixed some issues with tip visibility on mobile screens

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require("webpack");
+var path = require("path");
+
+var BUILD_DIR = path.join(__dirname, "dist");
+var APP_DIR = __dirname;
+
+var config = {
+    entry:  {
+        index: APP_DIR + "/index.js"
+    },
+    output: {
+        path: BUILD_DIR,
+        filename: "[name].bundle.js"
+    },
+    module: {
+        rules: [{
+            test : /\.js?$/,
+            exclude: /node_modules/,
+            use: {loader: "babel-loader"}
+        }]
+    },
+    devtool: "source-map",
+    devServer: {
+        host: "0.0.0.0",
+        publicPath: "/dist",
+        inline: true
+    }
+};
+
+module.exports = config;
