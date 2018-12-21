146ce985e1aebca802e65019dedfdc98e09fd7a4
cat <antik.fnt@gmail.com>
2018-12-21 17:35:31 +0300

    Updates

---
+++ precache-manifest.19a12440cd75e4acc89287f44d911d54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed919192c6f0daeeaea9",
+    "url": "/static/css/main.6e52ac05.chunk.css"
+  },
+  {
+    "revision": "ed919192c6f0daeeaea9",
+    "url": "/static/js/main.ed919192.chunk.js"
+  },
+  {
+    "revision": "4330481e77d2bb1dbcdb",
+    "url": "/static/js/1.4330481e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a7a72979a3d2e1af3c0660975cda857d",
+    "url": "/static/media/bg.min.a7a72979.jpg"
+  },
+  {
+    "revision": "b5ac7be01acfba59baeb280f4aea223e",
+    "url": "/static/media/bg_ny.min.b5ac7be0.jpg"
+  },
+  {
+    "revision": "8edee48152b751e4846d845abf40fab5",
+    "url": "/index.html"
+  }
+];
