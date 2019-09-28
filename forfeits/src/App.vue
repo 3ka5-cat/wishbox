9221fdd3bb1d29d8b4ee2bb2850858a1c8b26472
Anton Aleksanenkov <antik.fnt@gmail.com>
2019-09-28 23:55:08 +0300

    Add forfeits page

---
+++ forfeits/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <div id="spinner" v-if="loading">
+      <flower-spinner
+      :animation-duration="2500"
+      :size="70"
+      color="#FFCF40"
+      />
+    </div>
+    <WelcomeMessage v-if="!isPlaying"/>
+    <Forfeit v-else></Forfeit>
+  </div>
+</template>
+
+<script>
+  import { FlowerSpinner } from 'epic-spinners'
+  import { mapState } from 'vuex'
+  import Forfeit from './components/Forfeit.vue'
+  import WelcomeMessage from './components/WelcomeMessage.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      FlowerSpinner,
+      Forfeit,
+      WelcomeMessage
+    },
+    computed: mapState([
+      'isPlaying',
+      'loading',
+    ])
+  }
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:500,400,300&subset=latin,cyrillic');
+
+html {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+}
+
+body {
+  background: #45C7D5 url(./assets/bg.png) no-repeat scroll 0 -50px;
+  /*height: 100%;*/
+  margin: 0;
+  padding: 0;
+  color: #fff;
+  font-family: 'Open Sans', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+#app {
+  height: 100%;
+}
+#spinner {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-left: -50px;
+  margin-top: -50px;
+}
+</style>
---
+++ forfeits/src/components/Forfeit.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container h-100">
+    <div class="col">
+      <div id="header">
+        <h1>
+          Как вам такое задание?
+        </h1>
+      </div>
+      <div style="margin-top: 70px; font-size: 25pt;">
+        {{ forfeit.text }}
+      </div>
+      <div style="margin-top: 70px;">
+        <button type="button" class="btn btn-success btn-circle"
+                @click="SEND_FEEDBACK({id: forfeit.id, feedback: 'like'}); SET_FORFEIT()">
+          <v-icon scale=1.8 name="heart"/>
+        </button>
+        <button type="button" class="btn btn-danger btn-circle"
+                @click="SEND_FEEDBACK({id: forfeit.id, feedback: 'dislike'}); SET_FORFEIT()">
+          <v-icon scale=1.8 name="heart-broken"/>
+        </button>
+      </div>
+      <div style="margin-top: 70px;" >Кстати, вы можете предложить свое задание и поучаствовать в рейтинге</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Icon from 'vue-awesome/components/Icon'
+  import {mapState, mapActions} from 'vuex'
+
+  export default {
+    name: 'Forfeit',
+    components: {
+      'v-icon': Icon
+    },
+    computed: mapState([
+      'forfeit',
+    ]),
+    methods: {
+      ...mapActions([
+        'SET_FORFEIT',
+        'SEND_FEEDBACK',
+      ]),
+    }
+  }
+</script>
+
+<style scoped>
+.btn-circle {
+    width: 60px;
+    height: 60px;
+    padding: 12px 0px;
+    border-radius: 30px;
+    margin: 0 30px;
+}
+</style>
